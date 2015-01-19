5c18460379fb1fa3174386424fde9c32b1b04933
Bruce Becker <brucellino@gmail.com>
2015-01-19 07:47:58 +0100

    moved REMOTE_VERSION outside of if statement and added a few debugging echos

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ echo "Source dir is $SRC_DIR"
 CPUS=$(cat /proc/cpuinfo |grep "^processor"|wc -l)
 module add ci
 module add gcc/4.8.2
-
+REMOTE_VERSION=`echo $VERSION | sed "s/\\./\_/g"`
 # Direct link from TENET is http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz?use_mirror=tenet
 if [[ ! -s $SRC_DIR/$SOURCE_FILE ]] ; then
   echo "tarball's not here ! let's get it"
   mkdir -p $SRC_DIR
-  REMOTE_VERSION=`echo $VERSION | sed "s/\\./\_/g"`
+  echo $REMOTE_VERSION
 #  wget http://sourceforge.net/projects/boost/files/boost/$VERSION/${NAME}_${REMOTE_VERSION}.tar.gz/download -O $SRC_DIR/$SOURCE_FILE
   echo "Getting it from sourceforge with the following command:"
   echo " wget http://downloads.sourceforge.net/project/$NAME/$NAME/$VERSION/${NAME}_${REMOTE_VERSION}.tar.gz -O $SRC_DIR/$SOURCE_FILE"
@@ -20,9 +20,10 @@ if [[ ! -s $SRC_DIR/$SOURCE_FILE ]] ; then
   ls -lht $SRC_DIR
 fi
 
-tar xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+tar xzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+ls $WORKSPACE
 # this creates boost_1_55_0 | we would like it to follow our "." naming conventions
-mv $WORKSPACE/${NAME}_${REMOTE_VERSION} $WORKSPACE/$NAME-$VERSION
+mv $WORKSPACE/${NAME}_${{ REMOTE_VERSION }} $WORKSPACE/$NAME-$VERSION
 cd $WORKSPACE/$NAME-$VERSION
 ./bootstrap.sh --prefix=$SOFT_DIR
 ./b2 -d+2 stage threading=multi link=shared
