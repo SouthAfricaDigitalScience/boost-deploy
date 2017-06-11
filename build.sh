121a916a74ee79992681deccebac59f4de0d7fea
Bruce Becker <brucellino@gmail.com>
2017-06-11 11:52:33 +0200

    pass ALL the python options

--- build.sh
+++ build.sh
@@ -49,9 +49,9 @@ cd ${WORKSPACE}/${NAME}_${REMOTE_VERSION}
 echo "Making mpi bindings"
 echo "using mpi ;" >> project-config.jam
 echo "Making python bindings"
-echo "using python : 2.7 : ${PYTHON_DIR} ;" >> project-config.jam
+echo "using python : 2.7 : ${PYTHONHOME}/bin/python : ${PYTHONHOME}/include/python2.7  : ${PYTHONHOME}/lib/python2.7 ;" >> project-config.jam
 ./b2 -d+2 stage \
-threading=multi link=shared runtime-link=shared \
+threading=multi link=static,shared runtime-link=shared,shared \
   -sMPI_PATH=${OPENMPI_DIR} --debug-configuration \
   -sBZIP2_BINARY=bz2 -sBZLIB_INCLUDE=${BZLIB_DIR}/include -sBZLIB_LIBDIR=${BZLIB_DIR}/lib \
   --prefix=${SOFT_DIR}/${VERSION}-mpi-${OPENMPI_VERSION}-gcc-${GCC_VERSION} \
